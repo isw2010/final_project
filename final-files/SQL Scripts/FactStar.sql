4712a89c6bcf03f33397163bfe4edf3594a2f3a4
isw2010 <isw2010@gmail.com>
2021-06-16 00:41:54 +0300

    update file

--- final-files/SQL Scripts/FactStar.sql
+++
@@ -1,9 +0,0 @@
-
-truncate table [DIM].[DimCallOriginType]
-truncate table [DIM].[DimCallTypes]
-truncate table [DIM].[DimCountries]
-truncate table [DIM].[DimCustomers]
-truncate table [DIM].[DimCustomers]
-truncate table [DIM].[DimDate]
-truncate table [DIM].[DimOperators]
-truncate table [DIM].[DimTime]
